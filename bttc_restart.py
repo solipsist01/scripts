2f1ec359454fec20c1bd0d6ba3ea9d62bca438a9
ronald <wesselingronald@gmail.com>
2022-04-27 12:23:21 +0200

    commit

--- bttc_restart.py
+++ bttc_restart.py
@@ -1,19 +1,28 @@
 import os
 import docker
+import time
+import asyncio
 
-client = docker.from_env()
-client.images.pull('solipsist01/go-btfs:latest')
-for container in client.containers.list():
-    if "btfs" in container.name:
-        mounts = container.attrs['Mounts']
-        for mount in mounts:
-            if "btfs" in mount['Source']:
+def background(f):
+    def wrapped(*args, **kwargs):
+        return asyncio.get_event_loop().run_in_executor(None, f, *args, **kwargs)
+
+    return wrapped
+
+@background
+def restart_containe(container):
+    status = 0
+    mounts = container.attrs['Mounts']
+    for mount in mounts:
+        if "btfs" in mount['Source']:
+            while status == 0:
                 directory = mount['Source']
                 lockfile = directory + '/repo.lock'
                 chmod = 'chmod 777 ' + directory + ' -R'
 
                 print("Stopping container: " + container.name)
                 container.stop()
+                time.sleep(1)
 
                 print("Applying recursive chmod for directory: " + directory)
                 os.system(chmod)
@@ -28,5 +37,21 @@ for container in client.containers.list():
                 print("Starting container: " + container.name)
                 container.start()
 
-        
+                print("Waiting 30 seconds for container: " + container.name)
+                time.sleep(30)
 
+                print("Checking if container " + container.name + " started successfully.")
+
+                for line in container.logs(stream=True, follow=False):
+                    #this is infact the last line from the logs.
+                    data = str((line.strip()))
+
+                if 'Current host settings will be synced' in data:
+                    print(container.name + " started successfully")
+                    status = 1
+
+client = docker.from_env()
+client.images.pull('solipsist01/go-btfs:latest')
+for container in client.containers.list():
+    if "btfs" in container.name:
+        restart_containe(container)
